953925735d7223db1b470d0f18452f1900290215
nnattawat <armmer1@gmail.com>
2017-03-23 16:14:26 +1100

    Add new textarea box for message

--- public/app.jsx
+++ public/app.jsx
@@ -1,15 +1,33 @@
 var GreeterForm = React.createClass({
   formSubmit: function(e) {
     e.preventDefault();
+
+    var updates = {};
+
     var name = this.refs.name.value;
-    this.refs.name.value = '';
-    this.props.onNewName(name);
+    if (name.length > 0) {
+      updates.name = name;
+      this.refs.name.value = '';
+    }
+
+    var message = this.refs.message.value;
+    if (message.length > 0) {
+      updates.message = message;
+      this.refs.message.value = '';
+    }
+
+    this.props.onUpdates(updates);
   },
 
   render: function() {
     return (
       <form onSubmit={this.formSubmit}>
-        <input type="text" ref="name"/>
+        <div>
+          <input type="text" ref="name"/>
+        </div>
+        <div>
+          <textarea ref="message"></textarea>
+        </div>
         <button>Set name</button>
       </form>
     );
@@ -45,17 +63,15 @@ var Greeter = React.createClass({
     }
   },
 
-  handleNewName: function(name) {
-    this.setState({
-      name: name
-    });
+  handleUpdates: function(updates) {
+    this.setState(updates);
   },
 
   render: function() {
     return (
       <div>
         <GreeterMessage name={this.state.name} message={this.state.message}/>
-        <GreeterForm onNewName={this.handleNewName}/>
+        <GreeterForm onUpdates={this.handleUpdates}/>
       </div>
     );
   }
