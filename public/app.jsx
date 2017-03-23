b121fc565b680217130d8b8c216eeb337b2ca133
nnattawat <armmer1@gmail.com>
2017-03-23 15:09:48 +1100

    Using property with default value

--- public/app.jsx
+++ public/app.jsx
@@ -1,17 +1,23 @@
 var Greeter = React.createClass({
+  getDefaultProps: function() {
+    return {
+      name: 'Default Name',
+      message: '...'
+    }
+  },
   render: function() {
+    var name = this.props.name;
+    var message = this.props.message;
     return (
       <div>
-        <h1>Hello React</h1>
-        <p>
-          This is the simple React app
-        </p>
+        <h1>Hello {name}!</h1>
+        <p>{message}</p>
       </div>
     );
   }
 });
 
 ReactDOM.render(
-  <Greeter/>,
+  <Greeter message="This is a first react app"/>,
   document.getElementById('app')
 );
