dc48d9674ee640f8c23c63994958acd9866e5bc0
nnattawat <armmer1@gmail.com>
2017-03-23 16:00:51 +1100

    Extract compoments into sub component

--- public/app.jsx
+++ public/app.jsx
@@ -1,3 +1,35 @@
+var GreeterForm = React.createClass({
+  formSubmit: function(e) {
+    e.preventDefault();
+    var name = this.refs.name.value;
+    this.refs.name.value = '';
+    this.props.onNewName(name);
+  },
+
+  render: function() {
+    return (
+      <form onSubmit={this.formSubmit}>
+        <input type="text" ref="name"/>
+        <button>Set name</button>
+      </form>
+    );
+  }
+});
+
+var GreeterMessage = React.createClass({
+  render: function() {
+    var name = this.props.name;
+    var message = this.props.message
+    return (
+      <div>
+        <h1>Hello {name}!</h1>
+        <p>{message}</p>
+      </div>
+    );
+  }
+
+});
+
 var Greeter = React.createClass({
   getDefaultProps: function() {
     return {
@@ -13,33 +45,23 @@ var Greeter = React.createClass({
     }
   },
 
-  formSubmit: function(e) {
-    e.preventDefault();
-    var name = this.refs.name.value;
-    this.refs.name.value = '';
+  handleNewName: function(name) {
     this.setState({
       name: name
     });
   },
 
   render: function() {
-    var name = this.state.name;
-    var message = this.state.message;
     return (
       <div>
-        <h1>Hello {name}!</h1>
-        <p>{message}</p>
-
-        <form onSubmit={this.formSubmit}>
-          <input type="text" ref="name"/>
-          <button>Set name</button>
-        </form>
+        <GreeterMessage name={this.state.name} message={this.state.message}/>
+        <GreeterForm onNewName={this.handleNewName}/>
       </div>
     );
   }
 });
 
 ReactDOM.render(
-  <Greeter message="This is a first react app"/>,
+  <Greeter />,
   document.getElementById('app')
 );
